4988356bc8b7260f56c6d3d34462cdaf2834ad99
343784815 <343784815@C-5Y38GQ1.tdsb.on.ca>
2016-09-14 12:41:36 -0400

    AboutMe

---
+++ AboutME/src/agustin/AboutMe.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package agustin;
+
+/**
+ * @author 343784815
+ *
+ */
+public class AboutMe {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Albert Jay Agustin");
+		System.out.println("Mrs. Kemp");
+		System.out.println("Runnymede C.I");
+		System.out.println("\"GO RAVENS\"");
+	}
+
+}
