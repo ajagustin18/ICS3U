1599e6372a4ffe52219906b5a0fbe4d6ca3767d3
343784815 <343784815@C-5Y38GQ1.tdsb.on.ca>
2016-09-14 13:11:17 -0400

    AboutMe #2

--- AboutME/src/agustin/AboutMe.java
+++ AboutME/src/agustin/AboutMe.java
@@ -15,9 +15,15 @@ public class AboutMe {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Albert Jay Agustin");
-		System.out.println("Mrs. Kemp");
+		System.out.println("Ms. Kemp");
 		System.out.println("Runnymede C.I");
-		System.out.println("\"GO RAVENS\"");
+		System.out.println("\"GO RAVENS\"\n\n");
+		
+		System.out.format("%-10s %14s %18s","Weekdays","ClassCode","Time\n\n");
+		System.out.format("%-10s %8s %20s","Monday-Friday\t","SPH3U1-01","08:45 am\n");
+		System.out.format("%-10s %8s %20s","Monday-Friday\t","SCH3U1-01","10:05 am\n");
+		System.out.format("%-10s %8s %17s","Monday-Friday\t","ICS3C/3U1-01","12:26 pm\n");
+		System.out.format("%-10s %8s %20s","Monday-Friday\t","MCF3M1-01","01:45 pm\n");
 	}
 
 }
