7cb8ae68e948ea75a736739d24a1bdea5096004f
343784815 <343784815@C-5Y38GQ1.tdsb.on.ca>
2016-09-20 13:39:38 -0400

    BinaryNumber

--- AboutME/src/agustin/BinaryNumbers.java
+++ AboutME/src/agustin/BinaryNumbers.java
@@ -15,26 +15,26 @@ public class BinaryNumbers {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.format("%-7s %15s%15s", "Decimal", "Binary","Hexadecimal\n");
-		System.out.format("%7s%14s%16s", "1","0","0\n");
-		System.out.format("%7s%14s%16s", "2","1","1\n");
-		System.out.format("%7s%14s%16s", "3","10","2\n");
-		System.out.format("%7s%14s%16s", "4","11","3\n");
-		System.out.format("%7s%14s%16s", "5","100","4\n");
-		System.out.format("%7s%14s%16s", "6","101","5\n");
+		System.out.format("%7s%14s%16s", "1","01","0\n");
+		System.out.format("%7s%14s%16s", "2","10","1\n");
+		System.out.format("%7s%14s%16s", "3","11","2\n");
+		System.out.format("%7s%14s%16s", "4","100","3\n");
+		System.out.format("%7s%14s%16s", "5","101","4\n");
+		System.out.format("%7s%14s%16s", "6","110","5\n");
 		System.out.format("%7s%14s%16s", "7","111","6\n");
 		System.out.format("%7s%14s%16s", "8","1000","7\n");
 		System.out.format("%7s%14s%16s", "9","1001","8\n");
-		System.out.format("%7s%14s%16s", "10","1011","9\n");
-		System.out.format("%7s%14s%16s", "11","1111","A\n");
-		System.out.format("%7s%14s%16s", "12","10000","B\n");
-		System.out.format("%7s%14s%16s", "13","10001","C\n");
-		System.out.format("%7s%14s%16s", "14","10010","D\n");
-		System.out.format("%7s%14s%16s", "15","10011","E\n");
-		System.out.format("%7s%14s%16s", "16","10100","F\n");
-		System.out.format("%7s%14s%16s", "17","10101","10\n");
-		System.out.format("%7s%14s%16s", "18","10110","11\n");
-		System.out.format("%7s%14s%16s", "19","10111","12\n");
-		System.out.format("%7s%14s%15s", "20"," 11000","13");
+		System.out.format("%7s%14s%16s", "10","1010","9\n");
+		System.out.format("%7s%14s%16s", "11","1011","A\n");
+		System.out.format("%7s%14s%16s", "12","1100","B\n");
+		System.out.format("%7s%14s%16s", "13","1101","C\n");
+		System.out.format("%7s%14s%16s", "14","1110","D\n");
+		System.out.format("%7s%14s%16s", "15","1111","E\n");
+		System.out.format("%7s%14s%16s", "16","10000","F\n");
+		System.out.format("%7s%14s%16s", "17","10001","10\n");
+		System.out.format("%7s%14s%16s", "18","10010","11\n");
+		System.out.format("%7s%14s%16s", "19","10011","12\n");
+		System.out.format("%7s%14s%15s", "20","10101","13");
 		
 		
 		
