29d3318bb4790523bab5335f67e30b812aa037f1
343784815 <343784815@C-5Y38GQ1.tdsb.on.ca>
2016-09-30 12:37:50 -0400

    ArithmaticExpressions

---
+++ AboutME/src/agustin/ArithmaticExpressions.java
@@ -0,0 +1,47 @@
+package agustin;
+/**
+ * 
+ */
+
+/**
+ * @author 343784815
+ *
+ */
+import java.util.Scanner;
+public class ArithmaticExpressions {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner (System.in);
+		
+		int num1;
+		int num2;
+		int num3;
+		int num4;
+		int num5;
+		
+		num1 = scan.nextInt();
+		num2 = scan.nextInt();
+		num3 = scan.nextInt();
+		num4 = scan.nextInt();
+		num5 = scan.nextInt();
+		
+		System.out.println("The Sum of Five Numbers are = "+ (num1+num2+num3+num4+num5));
+		System.out.println("The Sum of the second number being subtracted from the third is = "+ (num2-num3));
+		System.out.println("The Product of the First and Fifth number = "+ (num1*num5));
+		System.out.println("The Quotient of the fourth number divided by the second = "+ (num4/num2));
+		System.out.println("The Remainder when dividing the fourth number by the second = "+ (num4%num2));
+		System.out.println("The first number raised to the power of the third number = "+ (Math.pow(num1, num3)));
+		System.out.println("The square root of the fifth number = "+ (Math.sqrt(num5)));
+		
+		
+		
+		
+
+	}
+
+}
