a2acb3d0e75f78bc3b1c244302170f73ff031a18
343784815 <343784815@C-5Y38GQ1.tdsb.on.ca>
2016-10-12 13:17:09 -0400

    Order

---
+++ AboutME/src/agustin/Order.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package agustin;
+import java.text.DecimalFormat;
+/**
+ * @author 343784815
+ *
+ */
+import java.util.Scanner;
+public class Order {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat ("0.00");
+		
+		double burgers;
+		double fries;
+		double sodas;
+		double beforetax;
+		double tax;
+		double tendered;
+		double total;
+
+		System.out.print("Enter the number of burgers:" );
+		burgers=scan.nextDouble()*1.69;
+		
+		System.out.print("Enter the number of fries:" );
+		fries=scan.nextDouble()*1.09;
+		
+		System.out.print("Enter the number of sodas:" );
+		sodas=scan.nextDouble()*0.99;
+		
+		System.out.println("Total before tax: $" + (df.format(burgers+fries+sodas)));
+		beforetax=burgers+fries+sodas;
+		
+		System.out.println("Tax: $" + (df.format(beforetax * 0.065)));
+		tax=beforetax * 0.065;
+				
+		System.out.println("Total: $" + (df.format(beforetax+tax)));
+		total=beforetax+tax;
+		
+		System.out.print("Enter Amount tendered: $");
+		tendered=scan.nextDouble();
+		
+		System.out.println("Change: $" + (df.format(tendered-total)));
+		
+		
+		
+	}
+
+}
