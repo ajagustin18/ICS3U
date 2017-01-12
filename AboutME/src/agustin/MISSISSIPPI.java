af9d6c920e492463f8d0f7c9bbf6dc0c4f6552d5
343784815 <343784815@C-5Y38GQ1.tdsb.on.ca>
2017-01-12 12:22:47 -0500

    Mississippi

---
+++ AboutME/src/agustin/MISSISSIPPI.java
@@ -0,0 +1,79 @@
+/**
+ * 
+ */
+package agustin;
+
+/**
+ * @author 343784815
+ *Albert Jay Agustin
+ *January 11/2017
+ *MISSISSIPPI.java
+ *Write a program to spell out MISSISSIPPI using block letters
+ */
+public class MISSISSIPPI {
+
+	public static void BigM(){
+		System.out.println("M     M");
+		System.out.println("MM   MM");
+		System.out.println("M M M M");
+		System.out.println("M  M  M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+	}
+	public static void BigS(){
+		System.out.println(" SSSSS ");
+		System.out.println("S     S ");
+		System.out.println("S");
+		System.out.println(" SSSSS ");
+		System.out.println("      S ");
+		System.out.println("S     S");
+		System.out.println(" SSSSS ");
+	}
+	public static void BigI(){
+		System.out.println("IIIII");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("IIIII");
+	}
+	public static void BigP(){
+		System.out.println("PPPPP ");
+		System.out.println("P    P");
+		System.out.println("P    P");
+		System.out.println("P    P");
+		System.out.println("PPPPP ");
+		System.out.println("P");
+		System.out.println("P");
+		System.out.println("P");
+	}
+	
+	
+	public static void main(String[] args) {
+
+		BigM();
+		System.out.println();
+		BigI();
+		System.out.println();
+		BigS();
+		System.out.println();
+		BigS();
+		System.out.println();
+		BigI();
+		System.out.println();
+		BigS();
+		System.out.println();
+		BigS();
+		System.out.println();
+		BigI();
+		System.out.println();
+		BigP();
+		System.out.println();
+		BigP();
+		System.out.println();
+		BigI();
+	}
+
+}
